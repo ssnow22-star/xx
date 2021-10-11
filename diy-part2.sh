ba4e08a50c1ea645b12cf07d8af4d47c30b438ba
ssnow22-star <74156772+ssnow22-star@users.noreply.github.com>
2021-10-12 07:39:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,23 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+# 删除原smartdns核心
+#rm -rf package/feeds/packages/smartdns
+
+# 拉取smartdns核心
+#svn co https://github.com/coolsnowwolf/packages/trunk/net/smartdns package/feeds/packages/smartdns
+
+# 删除自带xray-core核心
+rm -rf package/feeds/packages/xray-core
+
+# 拉取passwall源码
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/lxhao61/openwrt-passwall.git package/passwall
+
+# 删除passwall中xray-core核心
+#rm -rf package/passwall/xray-core
+
+# 拉取ipsec-vpnd LuCI
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ipsec-vpnd package/lean/luci-app-ipsec-vpnd
